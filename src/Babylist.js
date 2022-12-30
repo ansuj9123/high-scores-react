54fd708e83229026129042f4d4835baca9219101
Ansu Jammeh <ansuj9@yahoo.com>
2022-12-30 17:47:46 +0000

    Level3 completed

--- src/Babylist.js
+++ src/Babylist.js
@@ -14,31 +14,84 @@ let sortedNames = babyNamesData.sort(function (a, b) {
 
     function Babylist() {
         const [names, setNames] = useState(sortedNames)
+        const [favouriteNames, setFavouriteNames] = useState([])
+
+        function addFavourite (name){
+            setFavouriteNames([...favouriteNames,name])
+
+            let newNames = names.filter((babyname)=>{
+                if(babyname.id!=name.id){
+                    return true
+                }
+            })
+            setNames(newNames)
+        }
+
+        function removeFavourite (name) {
+            setNames([...names, name]);
+
+            let newNames = favouriteNames.filter((babyname) => {
+                if (babyname.id != name.id) {
+                return true;
+                }
+            });
+            setFavouriteNames(newNames);
+        }
 
     
     return (
         <div>
-            <input onChange={(e) =>{
-                console.log(e.target.value)
-                let filterNames = sortedNames.filter((babyname)=>{
-                    if(babyname.name.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())){
-                        return true
-                    } 
-                })
-                console.log(filterNames)
-                setNames(filterNames)
-            }} type="text"/>
+            <input
+            onChange={(e) => {
+                console.log(e.target.value);
+                let filterNames = sortedNames.filter((babyname) => {
+                if (
+                    babyname.name
+                    .toLocaleLowerCase()
+                    .includes(e.target.value.toLocaleLowerCase())
+                ) {
+                    return true;
+                }
+                });
+                console.log(filterNames);
+                setNames(filterNames);
+            }}
+            type="text"
+            />
+
+            <h1>Favourite Names</h1>
+            <div className="name-container">
+            {favouriteNames.map((babyname) => {
+                return (
+                <div
+                    onClick={() => {
+                    removeFavourite(babyname)
+                    }}
+                    className={babyname.sex === "f" ? "female" : "male"}
+                >
+                    {babyname.name}{" "}
+                </div>
+                );
+            })}
+            </div>
+
+            <h1>Names</h1>
             <div className="name-container">
             {names.map((babyname) => {
                 return (
-                <div className={babyname.sex === "f" ? "female" : "male"}>
+                <div
+                    onClick={() => {
+                    addFavourite(babyname);
+                    }}
+                    className={babyname.sex === "f" ? "female" : "male"}
+                >
                     {babyname.name}{" "}
                 </div>
                 );
             })}
             </div>
         </div>
-    );
-    }
+        );
+        }
 
 export default Babylist;
